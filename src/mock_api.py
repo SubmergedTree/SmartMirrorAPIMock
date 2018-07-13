ce40eb65c303dfa7fd03dcf636ef2dd27fb5adc8
SubmergedTree <jannmann1@web.de>
2018-07-13 22:38:23 +0200

    status endpoint

--- src/mock_api.py
+++ src/mock_api.py
@@ -1,3 +1,19 @@
 from flask import Flask, jsonify, request
 
 
+app = Flask(__name__)
+port = 4000
+host = '0.0.0.0'
+
+
+def main():
+    app.run(host=host, port=port, debug=True)
+
+
+@app.route('/status', methods=["GET"])
+def status():
+    return jsonify({"status": "up"})
+
+
+if __name__ == "__main__":
+    main()
